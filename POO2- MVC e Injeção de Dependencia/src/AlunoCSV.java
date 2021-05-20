298c812c7d1b35cb7db39f2aacea1e3955b64728
Usuário <Usurio@192.168.15.5>
2021-05-20 12:21:57 -0400

    Ajustes!

--- POO2- MVC e Injeção de Dependencia/src/AlunoCSV.java
+++ POO2- MVC e Injeção de Dependencia/src/AlunoCSV.java
@@ -1,8 +1,6 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-
 public class AlunoCSV implements IAlunoPersist{
 	
 	public void gravar(Aluno alu) {
